10e6ca96d79f0aa75b7129da1b61a88da019e53c
«ChulakovEvgeniy» <>
2023-04-02 23:21:59 +0300

    решение 23 задачи

---
+++ zadacha23/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+Console.Clear();
+System.Console.WriteLine("Введите число до которого все числа необходимо возвести в куб :");
+int num = int.Parse(Console.ReadLine());
+int i = 0;
+
+while (i <= num)
+{
+    Console.Write($"{Math.Pow(i, 3)} ");
+    i++;
+}
+
+
+// почему-то не работает через for
+// for (int i = 1; i<=num; i++)
+// {
+//     Console.Write($"{Math.Pow(i, 3)} ");
+// }
+
